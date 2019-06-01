b01b9327aed3160c692605de609658dcb92b50fc
Jacky Hung <jacky5559tw@gmail.com>
2019-06-02 02:05:35 +0800

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,9 @@
+$('#insert').hide()
+
 // DOM Ready
 $(() => {
 
     // Initialize
-    $('#product-list').empty()
-    $('#page').hide()
-    $('#insert').hide()
     $('#insertLink').on('click', function() {
         $('#index').hide()
         $('#insert').show()
@@ -20,7 +19,7 @@ $(() => {
         $('#product-list').empty();
         for (var i = start; i <= end; i++) {
             if (items[i] == null) {
-                return
+                break
             }
             newItem(items[i])
         }
@@ -33,64 +32,101 @@ $(() => {
         $p = $('<p>').attr('class', 'price').text('NT$ ' + item.price)
 
         $item = $('<div>').attr('class', 'item').append($img).append($h3).append($p)
-        $col = $('<div>').attr('class', 'col-*').append($item)
+        $col = $('<div>').attr('class', 'col').append($item)
 
         $('#product-list').append($col)
     }
 
-    var newPage = (n, page) => {
-        var pageNum = n / pageCount
-        pageNum = (n % pageCount != 0) ? pageNum + 1 : pageNum
-        pageNum = Math.floor(pageNum)
-
+    var newPage = (n, page, s) => {
         $('#page-number').empty()
+
+        // dlli
+        if (s == 1) {
+            $dla = $('<a>').attr('class', 'page-link').attr('href', '#').attr('tabindex', '-1').attr('aria-disabled', 'true').text('«')
+            $dlli = $('<li>').attr('class', 'page-item').addClass('disabled').append($dla)
+        } else {
+            $dla = $('<a>').attr('class', 'page-link').attr('href', '#').text('«')
+            $dlli = $('<li>').attr('class', 'page-item').append($dla)
+        }
+        $('#page-number').append($dlli)
 
         // lli
         if (page == 1) {
-            $la = $('<a>').attr('class', 'page-link').attr('href', '#').attr('tabindex', '-1').attr('aria-disabled', 'true').text('«')
+            $la = $('<a>').attr('class', 'page-link').attr('href', '#').attr('tabindex', '-1').attr('aria-disabled', 'true').text('‹')
             $lli = $('<li>').attr('class', 'page-item').addClass('disabled').append($la)
         } else {
-            $la = $('<a>').attr('class', 'page-link').attr('href', '#').text('«')
+            $la = $('<a>').attr('class', 'page-link').attr('href', '#').text('‹')
             $lli = $('<li>').attr('class', 'page-item').append($la)
         }
         $('#page-number').append($lli)
 
         // li
-        for (var i = 1; i <= pageNum; i++) {
+        for (var i = s * 5 - 4; i <= s * 5 && i <= n; i++) {
             $a = $('<a>').attr('class', 'page-link').attr('href', '#').text(i)
 
             $a.on('click', function() {
                 page = $(this).text()
                 showItems(page)
-                newPage(n, page)
+                newPage(n, page, s)
             })
-
-            var strActive = ((page == i) ? ' active' : '')
-            $li = $('<li>').attr('class', 'page-item' + strActive).append($a)
+            if (page == i) {
+                $li = $('<li>').attr('class', 'page-item active').addClass('disabled').append($a)
+            } else {
+                $li = $('<li>').attr('class', 'page-item').append($a)
+            }
             $('#page-number').append($li)
         }
 
         // rli
-        if (page == pageNum) {
-            $ra = $('<a>').attr('class', 'page-link').attr('href', '#').attr('tabindex', '-1').attr('aria-disabled', 'true').text('»')
+        if (page == n) {
+            $ra = $('<a>').attr('class', 'page-link').attr('href', '#').attr('tabindex', '-1').attr('aria-disabled', 'true').text('›')
             $rli = $('<li>').attr('class', 'page-item').addClass('disabled').append($ra)
         } else {
-            $ra = $('<a>').attr('class', 'page-link').attr('href', '#').text('»')
+            $ra = $('<a>').attr('class', 'page-link').attr('href', '#').text('›')
             $rli = $('<li>').attr('class', 'page-item').append($ra)
         }
         $('#page-number').append($rli)
 
+        // drli
+        if (s * 5 >= n) {
+            $dra = $('<a>').attr('class', 'page-link').attr('href', '#').attr('tabindex', '-1').attr('aria-disabled', 'true').text('»')
+            $drli = $('<li>').attr('class', 'page-item').addClass('disabled').append($dra)
+        } else {
+            $dra = $('<a>').attr('class', 'page-link').attr('href', '#').text('»')
+            $drli = $('<li>').attr('class', 'page-item').append($dra)
+        }
+        $('#page-number').append($drli)
+
         // on click
+        $dla.on('click', function() {
+            s--
+            page = s * 5
+            showItems(page)
+            newPage(n, page, s)
+        })
+
+
         $la.on('click', function() {
+            if ((page % 5) == 1) { s-- }
             page--
             showItems(page)
-            newPage(n, page)
+            newPage(n, page, s)
         })
 
         $ra.on('click', function() {
+            if ((page % 5) == 0) { s++ }
             page++
             showItems(page)
-            newPage(n, page)
+            newPage(n, page, s)
+        })
+
+        $dra.on('click', function() {
+            s++
+            page = (s * 5) - 4
+            if (page > n) { page = n }
+            showItems(page)
+            newPage(n, page, s)
+
         })
     }
 
@@ -103,9 +139,13 @@ $(() => {
                     $('#product-list').empty()
                     items = response.items
 
+                    var pageNum = items.length / pageCount
+                    pageNum = (items.length % pageCount != 0) ? pageNum + 1 : pageNum
+                    pageNum = Math.floor(pageNum)
+
                     showItems(1)
                     $('#page').show()
-                    newPage(items.length, 1)
+                    newPage(pageNum, 1, 1)
 
                 } else {
                     $('#message').text('No Related Item')
@@ -121,7 +161,7 @@ $(() => {
 
     $('#insertButton').on('click', function() {
 
-        // 取得商品資料
+        // Get data
         var data = {
                 result: true,
                 item: {
@@ -134,7 +174,6 @@ $(() => {
             // 新增商品
         $.post('https://js.kchen.club/B04109002/insert', data, function(response) {
             if (response) {
-                // 伺服器有回傳資料
                 if (response.result) {
                     $('#message').text('Upload Success')
                     $('#dialog').modal('show')
